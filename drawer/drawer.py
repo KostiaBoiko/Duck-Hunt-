208202bd843dcbc1fcbb7b7d072f5e804dd87265
goldi4ek <2002mdo@gmail.com>
2023-02-21 19:31:42 +0200

    update drawer, created start menu

--- drawer/drawer.py
+++ drawer/drawer.py
@@ -2,10 +2,18 @@ import pygame
 
 class Drawer:
 
-    def drawbg(self):
+    def __init__(self):
         SCREEN_WIDTH = 720
         SCREEN_HEIGHT = 480
 
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags=pygame.SCALED)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags=pygame.SCALED)
         background = pygame.image.load("assets/game/game_back.png").convert_alpha()
-        screen.blit(background, (0, 0))
+        self.screen.blit(background, (0, 0))
+
+    def menu(self):
+
+        pass
+    def start_menu(self):
+        background = pygame.image.load('assets/menu/background.png').convert_alpha()
+        self.screen.blit(background,(0,0))
+        pass
--- gameplay/gameplay.py
+++ gameplay/gameplay.py
@@ -4,11 +4,13 @@ class Gameplay:
 
     def game(self):
         run = True
-        Drawer.drawbg(self)
+        draw = Drawer()
         while run:
+            draw.start_menu()
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.type == pygame.K_ESCAPE:
+
                         pass
                 if event.type == pygame.QUIT:
                     run = False
